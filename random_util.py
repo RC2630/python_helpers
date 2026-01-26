7f392873ec49e1dcf8aa0e64f8b34cbb247f4cb0
Lucas Qin <lucasqin2002edu@gmail.com>
2026-01-25 22:19:30 -0500

    added random_elements()

--- random_util.py
+++ random_util.py
@@ -9,3 +9,18 @@ def random_element[T](seq: MutableSequence[T], remove: bool = False) -> T:
     if remove:
         del seq[random_index]
     return element
+
+def random_elements[T](seq: MutableSequence[T], count: int, remove: bool = False) -> list[T]:
+    if len(seq) < count:
+        raise ValueError("random_elements() cannot be used if length of sequence < count")
+    random_indices: list[int] = []
+    while len(random_indices) < count:
+        random_index: int = randint(0, len(seq) - 1)
+        if random_index not in random_indices:
+            random_indices.append(random_index)
+    elements: list[T] = [seq[random_index] for random_index in random_indices]
+    if remove:
+        random_indices.sort(reverse = True)
+        for random_index in random_indices:
+            del seq[random_index]
+    return elements
