bfaf1a00548aa4d9d72840ea696e3ee048ebda4b
Lucas Qin <lucasqin2002edu@gmail.com>
2026-04-10 06:10:33 -0400

    added agg utils

--- pandas_util.py
+++ pandas_util.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from typing import Any
+from typing import NamedTuple
 
 def resolve_merged_cells(df: pd.DataFrame) -> pd.DataFrame:
     df = df.copy()
@@ -10,3 +11,60 @@ def resolve_merged_cells(df: pd.DataFrame) -> pd.DataFrame:
                 df.loc[i, col] = last_values[col]
             last_values[col] = df.loc[i, col]
     return df
+
+# ---------------------------------------------------------------
+
+def clean_aggregation(
+    df: pd.DataFrame,
+    group_by_cols: str | list[str],
+    agg_func: str,
+    agg_col: str | None = None,
+    agg_result_colname: str | None = None
+) -> pd.DataFrame:
+    
+    group_by_cols_: list[str] = \
+        group_by_cols if isinstance(group_by_cols, list) else [group_by_cols]
+    agg_col_: str = \
+        agg_col if agg_col is not None else group_by_cols_[0]
+    agg_result_colname_: str = \
+        agg_result_colname if agg_result_colname is not None else \
+        (f"{agg_func}_{agg_col}" if agg_col is not None else agg_func)
+    
+    return (df
+        .groupby(group_by_cols_)
+        .agg({agg_col_: agg_func})
+        .rename(columns = {agg_col_: agg_result_colname_})
+        .reset_index())
+
+# ---------------------------------------------------------------
+
+class AggSpec(NamedTuple):
+
+    agg_col: str
+    agg_func: str
+    agg_result_colname: str = ""
+
+    def get_agg_result_colname(self) -> str:
+        if self.agg_result_colname != "":
+            return self.agg_result_colname
+        else:
+            return f"{self.agg_func}_{self.agg_col}"
+
+# ---------------------------------------------------------------
+        
+def clean_multiple_aggregation(
+    df: pd.DataFrame,
+    group_by_cols: str | list[str],
+    *agg_specs: AggSpec
+) -> pd.DataFrame:
+    
+    agg_dict: dict[str, str] = \
+        {agg_spec.get_agg_result_colname(): agg_spec.agg_func for agg_spec in agg_specs}
+    rename_dict: dict[str, Any] = \
+        {agg_spec.get_agg_result_colname(): df[agg_spec.agg_col] for agg_spec in agg_specs}
+    
+    return (df
+        .assign(**rename_dict)
+        .groupby(group_by_cols)
+        .agg(agg_dict)
+        .reset_index())
